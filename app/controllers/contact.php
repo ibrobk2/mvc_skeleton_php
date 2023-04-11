e21859b5b5d3ad7a25aae6a05087bfbf4a2d3630
Ibrahim Yusuf <ibrobk@gmail.com>
2023-04-11 17:09:26 +0100

    MVC model skeleton finished

---
+++ app/controllers/contact.php
@@ -0,0 +1,12 @@
+<?php
+class Contact extends Controller {
+    public function index(){
+        echo "Contact Page";
+    }
+
+    public function phone(){
+        echo "Contact By Phone";
+    }
+}
+
+?>
---
+++ app/views/home/index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "<h2>Hello, ".$data['name']."</h2>";
+
+?>
